95cb46dc6836f740b509a69734cc2df5b231b70a
Vitoria <vitoriahellen3004@gmail.com>
2020-10-01 18:54:44 -0300

    Resolução de exercicios

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace areaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorbase; 
+            double valoraltura;
+            double valorarea;
+
+
+            Console.WriteLine("Calcule a área de um triângulo retângulo:\n");
+            
+            Console.Write("Base:");
+            valorbase = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Altura:");
+            valoraltura = Convert.ToDouble(Console.ReadLine());
+            valorarea = (valorbase * valoraltura) / 2;
+            Console.WriteLine($"Area:{valorarea}");
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
